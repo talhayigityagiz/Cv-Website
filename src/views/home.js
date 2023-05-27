585a2042067e98ea9ed2e9df3c03e9947bcacf6e
talhayigityagiz <talhayigityagiz@gmail.com>
2023-05-27 05:42:51 +0300

    uupdate

--- src/views/home.js
+++ src/views/home.js
@@ -12,8 +12,9 @@ const Home = (props) => {
         <meta property="og:title" content="Portfolio Page" />
       </Helmet>
       <div data-role="Header" className="home-navbar-container">
+        <div id="hakkimda" className="home-container01"></div>
         <div className="home-navbar">
-          <img src="/logo.svg" alt="image" className="home-image" />
+          <img alt="image" src="/logo.svg" className="home-image" />
           <div className="home-links-container">
             <a href="#hakkimda" className="home-link Navbar-Link">
               About
@@ -32,7 +33,7 @@ const Home = (props) => {
             </svg>
           </div>
           <div data-role="MobileMenu" className="home-mobile-menu">
-            <div className="home-container01">
+            <div className="home-container02">
               <span className="Card-Heading home-heading">Logo</span>
               <div data-role="CloseMobileMenu" className="home-close-menu">
                 <svg viewBox="0 0 1024 1024" className="home-icon02">
@@ -51,33 +52,63 @@ const Home = (props) => {
       </div>
       <div className="home-section-separator"></div>
       <div className="home-section-separator1"></div>
-      <div className="home-container02">
+      <div className="home-container03">
         <div className="home-hero">
           <img alt="image" src="/photo-400w.jpg" className="home-image1" />
           <div className="home-hero-text-container">
-            <h1 className="home-heading1 Section-Heading">Talha Yiğit Yağız</h1>
-            <span className="home-text">
-              Hello, I&apos;m Talha Yiğit Yağız. I am studying Physics at Yıldız
-              Technical University. I am working to specialize in areas such as
-              computers and software.
-            </span>
+            <h1 className="home-heading1">Talha Yiğit Yağız</h1>
             <div className="home-cta-btn-container">
-              <a href="#hakkimda" className="home-cta-btn Anchor button">
-                <span className="home-text01">About</span>
-              </a>
-              <a href="#iletisim" className="home-cta-btn1 button">
+              <a href="#iletisim" className="home-cta-btn button">
                 Contact
               </a>
             </div>
+            <span className="home-text">
+              <span>
+                Hello, I&apos;m Talha Yiğit Yağız.
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <br></br>
+              <span>
+                I am studying Physics at
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <br></br>
+              <span>Yıldız Technical University.</span>
+              <br></br>
+              <span> I am working to specialize</span>
+              <br></br>
+              <span>
+                {' '}
+                in areas
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <span>such as computers</span>
+              <br></br>
+              <span> and software.</span>
+            </span>
+            <a href="#hakkimda" className="home-cta-btn1 Anchor button">
+              <span className="home-text13">About</span>
+            </a>
           </div>
         </div>
       </div>
       <div className="home-section-separator2"></div>
       <div className="home-section-separator3"></div>
-      <div id="hakkimda" className="home-container03"></div>
       <div className="home-hakkmda">
-        <strong className="home-text02">About</strong>
-        <span className="home-text03">
+        <strong className="home-text14 Section-Heading">About</strong>
+        <span className="home-text15">
           Hello, I&apos;m Talha Yiğit Yağız. I am studying Physics at Yıldız
           Technical University. I am working to specialize in areas such as
           computers and software.
@@ -87,21 +118,13 @@ const Home = (props) => {
       <div className="home-section-separator5"></div>
       <div id="deneyim" className="home-container04"></div>
       <div className="home-deneyim">
-        <span className="home-text04 Section-Heading">Experiences</span>
+        <span className="home-text16 Section-Heading">Experiences</span>
         <div className="home-steps">
           <div className="home-container05">
             <div className="home-step">
               <div className="home-container06">
-                <div className="home-container07">
-                  <svg viewBox="0 0 1024 1024" className="home-icon04">
-                    <path d="M726 682v-84h256v84q0 42-24 74t-62 46v180h-86v-180q-84-30-84-120zM554 86v170h86v256h-256v-256h86v-170q0-18 12-31t30-13 30 13 12 31zM896 256h86v256h-256v-256h84v-170q0-18 13-31t31-13 30 13 12 31v170zM42 682v-84h256v84q0 90-84 120v180h-86v-180q-38-14-62-46t-24-74zM384 682v-84h256v84q0 42-24 74t-62 46v180h-84v-180q-38-14-62-46t-24-74zM214 86v170h84v256h-256v-256h86v-170q0-18 12-31t30-13 31 13 13 31z"></path>
-                  </svg>
-                </div>
-                <div className="home-line"></div>
-              </div>
-              <div className="home-container08">
-                <h1 className="home-text05">Python Jump</h1>
-                <span className="home-text06">
+                <h1 className="home-text17">Python Jump</h1>
+                <span className="home-text18">
                   <span>
                     Lorem ipsum dolor sit amet, consectetur adipiscing.
                   </span>
@@ -111,17 +134,9 @@ const Home = (props) => {
               </div>
             </div>
             <div className="home-step1">
-              <div className="home-container09">
-                <div className="home-line1"></div>
-                <div className="home-container10">
-                  <svg viewBox="0 0 1024 1024" className="home-icon06">
-                    <path d="M170 256v426h684v-426h-684zM854 768h170v86h-1024v-86h170q-34 0-59-26t-25-60v-426q0-34 25-60t59-26h684q34 0 59 26t25 60v426q0 34-25 60t-59 26z"></path>
-                  </svg>
-                </div>
-              </div>
-              <div className="home-container11">
-                <h1 className="home-text09">Sky-Sec</h1>
-                <span className="home-text10">
+              <div className="home-container07">
+                <h1 className="home-text21">Sky-Sec</h1>
+                <span className="home-text22">
                   <span>
                     Lorem ipsum dolor sit amet, consectetur adipiscing.
                   </span>
@@ -131,18 +146,9 @@ const Home = (props) => {
               </div>
             </div>
             <div className="home-step2">
-              <div className="home-container12">
-                <div className="home-line2"></div>
-                <div className="home-container13">
-                  <svg viewBox="0 0 1024 1024" className="home-icon08">
-                    <path d="M576 736l96 96 320-320-320-320-96 96 224 224z"></path>
-                    <path d="M448 288l-96-96-320 320 320 320 96-96-224-224z"></path>
-                  </svg>
-                </div>
-              </div>
-              <div className="home-container14">
-                <h1 className="home-text13">YetGen</h1>
-                <span className="home-text14">
+              <div className="home-container08">
+                <h1 className="home-text25">YetGen</h1>
+                <span className="home-text26">
                   <span>
                     Lorem ipsum dolor sit amet, consectetur adipiscing.
                   </span>
@@ -151,38 +157,57 @@ const Home = (props) => {
                 </span>
               </div>
             </div>
-            <div className="home-step3"></div>
           </div>
         </div>
       </div>
       <div className="home-section-separator6"></div>
-      <div id="iletisim" className="home-container15"></div>
+      <div id="iletisim" className="home-container09"></div>
       <div className="home-subscribe">
-        <div className="home-container16">
-          <div className="home-heading-container">
-            <h1 className="home-text17 Section-Heading">Contact</h1>
-            <a
-              href="mailto:talhayigityagiz@gmail.com?subject=İletişim"
-              className="home-link08"
-            >
-              Mail: talhayigityagiz@gmail.com
-            </a>
-          </div>
-        </div>
+        <div className="home-container10"></div>
+        <h1 className="home-text29 Section-Heading">Contact</h1>
+        <a
+          href="mailto:talhayigityagiz@gmail.com?subject=İletişim"
+          className="home-link08"
+        >
+          Mail: talhayigityagiz@gmail.com
+        </a>
       </div>
       <div className="home-section-separator7"></div>
       <div className="home-footer-container">
         <div className="home-footer">
           <div className="home-social-links">
-            <svg viewBox="0 0 950.8571428571428 1024" className="home-icon11">
-              <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
-            </svg>
-            <svg viewBox="0 0 877.7142857142857 1024" className="home-icon13">
-              <path d="M713.143 73.143c90.857 0 164.571 73.714 164.571 164.571v548.571c0 90.857-73.714 164.571-164.571 164.571h-107.429v-340h113.714l17.143-132.571h-130.857v-84.571c0-38.286 10.286-64 65.714-64l69.714-0.571v-118.286c-12-1.714-53.714-5.143-101.714-5.143-101.143 0-170.857 61.714-170.857 174.857v97.714h-114.286v132.571h114.286v340h-304c-90.857 0-164.571-73.714-164.571-164.571v-548.571c0-90.857 73.714-164.571 164.571-164.571h548.571z"></path>
-            </svg>
-            <svg viewBox="0 0 877.7142857142857 1024" className="home-icon15">
-              <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
-            </svg>
+            <a
+              href="https://www.linkedin.com/in/talhayigityagiz/"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="home-link09"
+            >
+              <svg viewBox="0 0 1024 1024" className="home-icon04">
+                <path d="M384 384h177.106v90.782h2.532c24.64-44.194 84.958-90.782 174.842-90.782 186.946 0 221.52 116.376 221.52 267.734v308.266h-184.61v-273.278c0-65.184-1.334-149.026-96.028-149.026-96.148 0-110.82 70.986-110.82 144.292v278.012h-184.542v-576z"></path>
+                <path d="M64 384h192v576h-192v-576z"></path>
+                <path d="M256 224c0 53.019-42.981 96-96 96s-96-42.981-96-96c0-53.019 42.981-96 96-96s96 42.981 96 96z"></path>
+              </svg>
+            </a>
+            <a
+              href="https://instagram.com/talhayigityagiz"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="home-link10"
+            >
+              <svg viewBox="0 0 877.7142857142857 1024" className="home-icon08">
+                <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+              </svg>
+            </a>
+            <a
+              href="https://github.com/talhayigityagiz"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="home-link11"
+            >
+              <svg viewBox="0 0 1024 1024" className="home-icon10">
+                <path d="M512.008 12.642c-282.738 0-512.008 229.218-512.008 511.998 0 226.214 146.704 418.132 350.136 485.836 25.586 4.738 34.992-11.11 34.992-24.632 0-12.204-0.48-52.542-0.696-95.324-142.448 30.976-172.504-60.41-172.504-60.41-23.282-59.176-56.848-74.916-56.848-74.916-46.452-31.778 3.51-31.124 3.51-31.124 51.4 3.61 78.476 52.766 78.476 52.766 45.672 78.27 119.776 55.64 149.004 42.558 4.588-33.086 17.852-55.68 32.506-68.464-113.73-12.942-233.276-56.85-233.276-253.032 0-55.898 20.004-101.574 52.76-137.428-5.316-12.9-22.854-64.972 4.952-135.5 0 0 43.006-13.752 140.84 52.49 40.836-11.348 84.636-17.036 128.154-17.234 43.502 0.198 87.336 5.886 128.256 17.234 97.734-66.244 140.656-52.49 140.656-52.49 27.872 70.528 10.35 122.6 5.036 135.5 32.82 35.856 52.694 81.532 52.694 137.428 0 196.654-119.778 239.95-233.79 252.624 18.364 15.89 34.724 47.046 34.724 94.812 0 68.508-0.596 123.644-0.596 140.508 0 13.628 9.222 29.594 35.172 24.566 203.322-67.776 349.842-259.626 349.842-485.768 0-282.78-229.234-511.998-511.992-511.998z"></path>
+              </svg>
+            </a>
           </div>
           <div className="home-copyright-container">
             <span>
